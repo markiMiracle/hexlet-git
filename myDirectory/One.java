61319524a955e2a2a128279c5adaa37f2161d0ea
markimark <someonewhosome@list.ru>
2023-11-29 11:26:16 +0300

    add something

---
+++ myDirectory/One.java
@@ -0,0 +1,7 @@
+package myDirectory;
+
+public class One {
+	    public static void main(String[] args) {
+		            Two.greeting();
+			        }
+}
---
+++ myDirectory/Two.java
@@ -0,0 +1,7 @@
+package myDirectory;
+
+public class Two {
+	    public static void greeting() {
+		            System.out.println("Java for Brave");
+			        }
+}
